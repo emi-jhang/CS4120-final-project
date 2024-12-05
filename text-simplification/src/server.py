b0cebe0ba287251a0fd0603bdc5647a10f7ea40d
olivegaoden <oliviamail123@gmail.com>
2024-12-05 09:43:01 -0500

    connect to script

--- text-simplification/src/server.py
+++ text-simplification/src/server.py
@@ -1,9 +1,15 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from dummy_script import simplify_text
+from script import get_sentences
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+
+def get_first_simplified_sentence(results):
+    # Access the first item from the dictionary
+    _, (first_simplified_sentence, _) = next(iter(results.items()))
+    
+    return first_simplified_sentence
 
 @app.route('/')
 def home():
@@ -20,13 +26,14 @@ def simplify():
 
     try:
         # Simplify the text using your function
-        simplified_text = simplify_text(input_text)
+        results = get_sentences(input_text)
+        simplified_text = get_first_simplified_sentence(results)
+        # simplified_text = dummy(input_text)
 
         return jsonify({'simplified_text': simplified_text})
     except Exception as e:
         # Handle any unexpected errors
-        # return jsonify({'error': str(e)}), 500
-        return jsonify({'Hello', 500})
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
