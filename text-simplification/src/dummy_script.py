22078b1dcbfa14d95eee36087c2ec8a19038b649
olivegaoden <oliviamail123@gmail.com>
2024-12-05 01:18:33 -0500

    testing with dummy script

--- text-simplification/src/dummy_script.py
+++ text-simplification/src/dummy_script.py
@@ -1,2 +1,11 @@
+import os
+import pandas as pd
 def simplify_text(input_text):
-    return input_text
+    file_path = '../../CWID_NonNative_Regressor.joblib'
+
+    if os.path.exists(file_path):
+        # dev = pd.read_csv(file_path)
+
+        return("File exists")
+    else:
+        return("File or directory does not exist.")
